16383a45661e6e0ef8d30f4eacc252da49953fec
Mark Durrant <mark.j.durrant@gmail.com>
2017-04-03 12:32:17 +0100

    add untis to svg

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ with (paper) {
   paper.setup(new Size(a4.portrait.x, a4.portrait.y));
 
   var svg = project.exportSVG({ asString: true });
+
+  svg = util.addUnitsToSvg(svg, a4.units);
+
 	var html = '<!doctype html><html lang="en"><head><meta charset="utf-8"></head><body>' + svg + '</body></html>';
 
   util.writeToFile('./output/out.svg', svg);
--- util.js
+++ util.js
@@ -7,3 +7,15 @@ exports.writeToFile = function (file, content) {
     console.log('Saved ' + file + ' ✔');
   });
 }
+
+exports.addUnitsToSvg = function(svg, units) {
+  var rx = /width="([0-9]+)" height="([0-9]+)"/;
+
+  var attr = 'width="' + svg.match(rx)[1] + units +
+            '" height="' + svg.match(rx)[2] + units +
+            '" viewbox="0 0 ' + svg.match(rx)[1] + ' ' + svg.match(rx)[2] + '"';
+
+  svg = svg.replace(rx, attr);
+
+  return svg;
+}
