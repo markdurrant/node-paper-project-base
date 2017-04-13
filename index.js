87a41a5e3953d7a815c13d60645812d374fff49f
Mark Durrant <mark.j.durrant@gmail.com>
2017-04-13 16:03:42 +0100

    clean repo

--- index.js
+++ index.js
@@ -16,40 +16,6 @@ with (paper) {
     strokeColor: 'grey'
   });
 
-  var diaGroup = new Group();
-
-  var size = view.bounds.width - 30;
-
-  var diaCenter = view.bounds.center.add([0, - 42]);
-
-  var diamond = new Path({
-    segments: [
-      diaCenter.add([0, size / 2]),
-      diaCenter.add([- size / 2, 0]),
-      diaCenter.add([0, - size / 2]),
-      diaCenter.add([size / 2, 0])
-    ],
-    closed: true,
-    style: pen.thick.grey
-  });
-
-  diaGroup.addChild(diamond);
-
-  var rotation = 5;
-  var itterations = 40;
-
-  for (var i = 1; i < itterations; i++) {
-    diaGroup.addChild(
-      diamond
-        .clone()
-        .scale(1 - i * 1 / itterations)
-        .rotate(rotation * i)
-        .smooth({type: 'geometric', factor: i * 1 / itterations})
-    );
-  }
-
-  diaGroup.clone().scale(-1, 1).style = pen.thin.lightBlue;
-
   var svg = project.exportSVG({asString: true, matchShapes: true});
 
   fUil.outputToSvg('./svg/paperSVG.svg', svg, a4.units);
