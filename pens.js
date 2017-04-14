ae5bae4848fa7e1df819d272c69d36c685740492
Mark Durrant <mark.j.durrant@gmail.com>
2017-04-14 14:15:21 +0100

    refactor pens.js

--- pens.js
+++ pens.js
@@ -1,36 +1,49 @@
-exports.thick = {
-  black: {strokeWidth: 1.5, strokeColor: '#333', strokeCap: 'round'},
-  grey: {strokeWidth: 1.5, strokeColor: '#d2dcdd', strokeCap: 'round'},
-  brown: {strokeWidth: 1.5, strokeColor: '#563333', strokeCap: 'round'},
-  red: {strokeWidth: 1.5, strokeColor: '#fc384b', strokeCap: 'round'},
-  orange: {strokeWidth: 1.5, strokeColor: '#fe8e2c', strokeCap: 'round'},
-  yellow: {strokeWidth: 1.5, strokeColor: '#ffff8b', strokeCap: 'round'},
-  lightGreen: {strokeWidth: 1.5, strokeColor: '#91f269', strokeCap: 'round'},
-  darkGreen: {strokeWidth: 1.5, strokeColor: '#1a924d', strokeCap: 'round'},
-  lightBlue: {strokeWidth: 1.5, strokeColor: '#1598e9', strokeCap: 'round'},
-  darkBlue: {strokeWidth: 1.5, strokeColor: '#3740ae', strokeCap: 'round'},
-  purple: {strokeWidth: 1.5, strokeColor: '#7a27a8', strokeCap: 'round'},
-  pink: {strokeWidth: 1.5, strokeColor: '#f02dab', strokeCap: 'round'}
+var thick = {
+  black:       {strokeColor: '#333333'},
+  grey:        {strokeColor: '#d2dcdd'},
+  brown:       {strokeColor: '#563333'},
+  red:         {strokeColor: '#fc384b'},
+  orange:      {strokeColor: '#fe8e2c'},
+  yellow:      {strokeColor: '#ffff8b'},
+  lightGreen:  {strokeColor: '#91f269'},
+  darkGreen:   {strokeColor: '#1a924d'},
+  lightBlue:   {strokeColor: '#1598e9'},
+  darkBlue:    {strokeColor: '#3740ae'},
+  purple:      {strokeColor: '#7a27a8'},
+  pink:        {strokeColor: '#f02dab'}
 };
 
-exports.thin = {
-  black: {strokeWidth: 0.5, strokeColor: '#333', strokeCap: 'round'},
-  darkGrey: {strokeWidth: 0.5, strokeColor: '#6f6b78', strokeCap: 'round'},
-  lightGrey: {strokeWidth: 0.5, strokeColor: '#d8dfea', strokeCap: 'round'},
-  yellow: {strokeWidth: 0.5, strokeColor: '#ffff7d', strokeCap: 'round'},
-  brown: {strokeWidth: 0.5, strokeColor: '#503032', strokeCap: 'round'},
-  brownOrange: {strokeWidth: 0.5, strokeColor: '#b5683d', strokeCap: 'round'},
-  orange: {strokeWidth: 0.5, strokeColor: '#fe9b41', strokeCap: 'round'},
-  red: {strokeWidth: 0.5, strokeColor: '#fd4f64', strokeCap: 'round'},
-  lightPink: {strokeWidth: 0.5, strokeColor: '#f33bab', strokeCap: 'round'},
-  darkPink: {strokeWidth: 0.5, strokeColor: '#e14970', strokeCap: 'round'},
-  lightPurple: {strokeWidth: 0.5, strokeColor: '#e933ca', strokeCap: 'round'},
-  darkPurple: {strokeWidth: 0.5, strokeColor: '#8b46b9', strokeCap: 'round'},
-  darkBlue: {strokeWidth: 0.5, strokeColor: '#1b5dc2', strokeCap: 'round'},
-  midBlue: {strokeWidth: 0.5, strokeColor: '#239cee', strokeCap: 'round'},
-  lightBlue: {strokeWidth: 0.5, strokeColor: '#239cee', strokeCap: 'round'},
-  cyan: {strokeWidth: 0.5, strokeColor: '#9cfdf5', strokeCap: 'round'},
-  lightGreen: {strokeWidth: 0.5, strokeColor: '#8ff360', strokeCap: 'round'},
-  darkGreen: {strokeWidth: 0.5, strokeColor: '#22a05c', strokeCap: 'round'},
-  greenBrown: {strokeWidth: 0.5, strokeColor: '#76844e', strokeCap: 'round'}
+Object.keys(thick).forEach(function(color) {
+  thick[color].strokeWidth = 1.5;
+  thick[color].strokeCap = 'round';
+});
+
+var thin = {
+  black:       {strokeColor: '#333333'},
+  darkGrey:    {strokeColor: '#6f6b78'},
+  lightGrey:   {strokeColor: '#d8dfea'},
+  yellow:      {strokeColor: '#ffff7d'},
+  brown:       {strokeColor: '#503032'},
+  brownOrange: {strokeColor: '#b5683d'},
+  orange:      {strokeColor: '#fe9b41'},
+  red:         {strokeColor: '#fd4f64'},
+  lightPink:   {strokeColor: '#f33bab'},
+  darkPink:    {strokeColor: '#e14970'},
+  lightPurple: {strokeColor: '#e933ca'},
+  darkPurple:  {strokeColor: '#8b46b9'},
+  darkBlue:    {strokeColor: '#1b5dc2'},
+  midBlue:     {strokeColor: '#239cee'},
+  lightBlue:   {strokeColor: '#239cee'},
+  cyan:        {strokeColor: '#9cfdf5'},
+  lightGreen:  {strokeColor: '#8ff360'},
+  darkGreen:   {strokeColor: '#22a05c'},
+  greenBrown:  {strokeColor: '#76844e'}
 };
+
+Object.keys(thin).forEach(function(color) {
+  thin[color].strokeWidth = 0.5;
+  thin[color].strokeCap = 'round';
+});
+
+exports.thick = thick;
+exports.thin = thin;
