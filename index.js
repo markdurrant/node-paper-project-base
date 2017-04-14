0e853dc02dbcc0584f0e752b431b7bd0045abc63
Mark Durrant <mark.j.durrant@gmail.com>
2017-04-14 13:50:35 +0100

    add random seed lib & add date/time to file

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 var paper = require('paper');
 var _ = require('lodash');
+var seed = require('seed-random');
 
 var fUil = require('./fileUtils.js');
 var pUtil = require('./paperUtils.js');
-var a4 = require('./a4.js');
 
+var a4 = require('./a4.js');
 var pen = require('./pens.js');
+
+var mySeed = '';
+
+if(process.argv[2]) {
+  mySeed = process.argv[2];
+} else {
+  var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+  for (var s = 0; s < 10; s++) {
+    mySeed = mySeed + chars.charAt(Math.random() * chars.length);
+  }
+}
+
+var now = new Date();
+var dateStr = now.getFullYear() + '-' + now.getDate() + '-' + now.getHours() + '-' + now.getMinutes() + '-' + now.getSeconds();
+
+seed(mySeed, {global: true});
 
 with (paper) {
   paper.setup(new Size(a4.portrait.x, a4.portrait.y));
@@ -16,8 +34,17 @@ with (paper) {
     strokeColor: 'grey'
   });
 
+  for (var i = 0; i < 10; i++) {
+    new Path.Circle({
+      radius: 2,
+      x: Math.random() * view.bounds.width,
+      y: Math.random() * view.bounds.height,
+      style: pen.thin.orange
+    });
+  }
+
   var svg = project.exportSVG({asString: true, matchShapes: true});
 
-  fUil.outputToSvg('./svg/paperSVG.svg', svg, a4.units);
+  fUil.outputToSvg('./svg/' + mySeed + '-' + dateStr + '.svg', svg, a4.units);
   fUil.outputToHtml('./index.html', svg, a4.units);
 }
