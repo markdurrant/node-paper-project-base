cabb1d4f588bd7aa699d334b1fada7f320e61aed
Mark Durrant <mark.j.durrant@gmail.com>
2017-05-20 02:34:02 +0100

    impliment voroni

--- index.js
+++ index.js
@@ -7,8 +7,9 @@ var seedUtil = require('./utilities/seedUtils.js');
 var page = require('./config/pageConfig.js');
 var pen = require('./config/pensConfig.js');
 
-var intersect = require('./drawing/lineIntersect.js');
-var cutOut = require('./drawing/lineCutOut.js');
+var getRandomPoint = require('./drawing/randomPoint.js');
+
+var Voronoi = require('voronoi');
 
 var seed = seedUtil.getSeed(process.argv[2]);
 
@@ -17,27 +18,43 @@ var seed = seedUtil.getSeed(process.argv[2]);
 with (paper) {
   paper.setup(new Size(page.x, page.y));
 
-  new Path.Rectangle({
+  var pageOutline = new Path.Rectangle({
     from: [0, 0],
-    to: view.bounds.bottomRight,
-    style: pen.thin.grey
+    to: view.bounds.bottomRight
   });
 
-  var mySquare = new Path.Rectangle({
-    from: view.bounds.center.subtract(view.bounds.width / 5),
-    to: view.bounds.center.add(view.bounds.width / 5),
-    style: pen.thin.lightGrey
+  var topLeft = new Point(20, 20);
+  var bottomRight = new Point(view.bounds.width - 20, view.bounds.height - 20);
+
+  var voronoi = new Voronoi();
+
+  var bbox = {xl: topLeft.x, xr: bottomRight.x, yt: topLeft.y, yb: bottomRight.y};
+
+  var sites = [];
+
+  for (var p = 0; p < 100; p++) {
+    sites.push(getRandomPoint(topLeft, bottomRight));
+  }
+
+  var diagram = voronoi.compute(sites, bbox);
+
+  console.log(diagram.edges);
+
+  diagram.vertices.forEach(function(vertice) {
+    new Path.Circle({
+      center: new Point(vertice.x, vertice.y),
+      radius: 1,
+      style: pen.thick.orange
+    });
   });
 
-  var myLine = new Path.Line({
-    from: view.bounds.center.subtract([0, 100]),
-    to: view.bounds.center.add([0, 100]),
-    style: pen.thin.cyan
+  diagram.edges.forEach(function(edge) {
+    new Path.Line({
+      from: new Point(edge.va.x, edge.va.y),
+      to: new Point(edge.vb.x, edge.vb.y),
+      style: pen.thick.lightBlue
+    });
   });
-
-  myLine.rotate(45, view.bounds.center);
-
-  cutOut(myLine, mySquare);
 
   var svg = project.exportSVG({asString: true});
 }
